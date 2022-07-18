38fb12800daba28f56efcdf326c1fadfa393b603
Nicolas Taccetta <nicolastaccetta@gmail.com>
2022-07-18 20:18:07 -0300

    firebase

--- src/Components/ItemDetailContainer/ItemDetailContainer.js
+++ src/Components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,21 +1,38 @@
 import React, { useEffect, useState } from "react";
 import ItemDetail from "../ItemDetail/ItemDetail.js";
 import Loading from "../../assets/img/Loading.gif";
-import { getProd } from "../../mocks/fakeApi";
+//import { getProd } from "../../mocks/fakeApi";
 import { useParams } from "react-router-dom";
+import { db } from '../../firebase/firebase';
+import { doc, getDoc, collection } from "firebase/firestore";;
+
 
 const ItemDetailContainer = () => {
   const [product, setProduct] = useState({});
   const [loading, setLoading] = useState(true);
-
   const { id } = useParams();
 
-  useEffect(() => {
-      getProd(id)
-      .then((result) => setProduct(result))
-      .catch((error) => console.log(error))
-      .finally(() => setLoading(false));
+  useEffect (() => {
+    const productsCollection = collection(db, 'product')
+    const refDoc = doc(productsCollection, id)
+    getDoc(refDoc).then(result => {
+      setProduct({
+        id: result.id,
+        ...result.data(),
+      })
+    })
+    .catch((error) => console.log(error))
+    .finally(() => setLoading(false));
   }, [id]);
+
+
+
+ //useEffect(() => {
+ //     getProd(id)
+ //     .then((result) => setProduct(result))
+ //     .catch((error) => console.log(error))
+ //     .finally(() => setLoading(false));
+ //  }, [id]);
 
   return (
     <div>
--- src/Components/itemListContainer/ItemListContainer.js
+++ src/Components/itemListContainer/ItemListContainer.js
@@ -1,21 +1,41 @@
 import React, { useEffect, useState } from "react";
 import ItemList from "../itemList/ItemList";
 import Loading from "../../assets/img/Loading.gif";
-import { getData } from "../../mocks/fakeApi";
+//import { getData, product } from "../../mocks/fakeApi";
 import { useParams } from "react-router-dom";
+import { db } from "../../firebase/firebase";
+import { getDocs, collection, query, where} from "firebase/firestore";
 
 const ItemListContainer = ({ greeting }) => {
   const [productList, setProductList] = useState([]);
   const [loading, setLoading] = useState(true);
-
   const { categoryId } = useParams();
 
   useEffect(() => {
-    setLoading(true)
-    getData(categoryId)
-      .then((result) => setProductList(result))
-      .catch((error) => console.log(error))
-      .finally(() => setLoading(false));
+    const productsCollection = collection(db, 'product');
+    const q = query(productsCollection, where('categoria', '==', 'categoryId'))
+
+    getDocs(categoryId
+      ? q
+      : productsCollection
+      )
+    .then(result => {
+      const lista = result.docs.map(product => {
+        return {
+          id: product.id,
+          ...product.data(),
+        } 
+      })
+      setProductList(lista);
+    })
+    .catch((error) => console.log(error))
+    .finally(() => setLoading(false));
+
+    //setLoading(true)
+    //getData(categoryId)
+      //.then((result) => setProductList(result))
+      //.catch((error) => console.log(error))
+      //.finally(() => setLoading(false));
   }, [categoryId]);
 
   return (
